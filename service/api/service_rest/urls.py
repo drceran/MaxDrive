2430e51a3b200d8b38c0356155ff8cdd35c6e939
lrningcode12 <117692502+lrningcode12@users.noreply.github.com>
2023-04-25 00:02:36 -0700

    Cleaned up url links

--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -10,10 +10,10 @@ from .views import (
 
 urlpatterns = [
     path("technicians/", technicians_list, name="technicians_list"),
-    path("technicians/<id:id>", technician_detail, name="technician_detail"),
+    path("technicians/<int:id>", technician_detail, name="technician_detail"),
     path("appointments/", appointments_list, name="appointments_list"),
-    path("appointments/<id:id>", appointments_detail, name="appointments_detail"),
-    path("appointments/<id:id>/cancel/", cancel_appointment, name="cancel_appointment"),
-    path("appointments/<id:id>/finish/", finished_appointment, name="finished_appointment"),
+    path("appointments/<int:id>", appointments_detail, name="appointments_detail"),
+    path("appointments/<int:id>/cancel/", cancel_appointment, name="cancel_appointment"),
+    path("appointments/<int:id>/finish/", finished_appointment, name="finished_appointment"),
 
 ]
