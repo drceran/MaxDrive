8aba12b592367b5148173b5cff9170f665b7ba74
lrningcode12 <117692502+lrningcode12@users.noreply.github.com>
2023-04-25 10:58:09 -0700

    Updated Appointments List GET and POST view

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,27 +1,43 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
-from .models import Technician, Appointment
+from .models import Technician, Appointment, AutomobileVO
 from common.json import ModelEncoder
 import json
 
-class TechnicianListEncoder(ModelEncoder):
+class TechnicianEncoder(ModelEncoder):
     model = Technician
     properties = [
+        "id",
         "employee_id",
         "first_name",
-        "last_name"
+        "last_name",
     ]
+
+class AppointmentEncoder(ModelEncoder):
+    model = Appointment
+    properties = [
+        "date_time",
+        "reason",
+        "status",
+        "vin",
+        "customer",
+        "technician"
+    ]
+
+    encoders={
+        "technician":TechnicianEncoder()
+    }
 
 @require_http_methods(["GET", "POST"])
 def technicians_list(request):
     if request.method == "GET":
         technicians = Technician.objects.all()
-        return JsonResponse({"technicians": technicians}, encoder=TechnicianListEncoder)
+        return JsonResponse({"technicians": technicians}, encoder=TechnicianEncoder)
     else: #IOW, POST
         content = json.loads(request.body)
         technician = Technician.objects.create(**content)
-        return JsonResponse(technician, encoder=TechnicianListEncoder, safe=False)
+        return JsonResponse(technician, encoder=TechnicianEncoder, safe=False)
 
 
 @require_http_methods(["DELETE"])
@@ -31,7 +47,19 @@ def technician_detail(request, id):
 
 @require_http_methods(["GET", "POST"])
 def appointments_list(request):
-    pass
+    if request.method == "GET":
+        appointments = Appointment.objects.all()
+        return JsonResponse({"appointments": appointments}, encoder=AppointmentEncoder)
+    else: #IOW POST
+        content = json.loads(request.body)
+        try:
+            technician_id = content["technician"]
+            technician = Technician.objects.get(id=technician_id)
+            content["technician"] = technician
+        except Technician.DoesNotExist:
+            return JsonResponse({"message": "technician does not exist"})
+        appointment = Appointment.objects.create(**content)
+        return JsonResponse(appointment, encoder=AppointmentEncoder, safe=False)
 
 @require_http_methods(["DELETE"])
 def appointments_detail(request, id):
