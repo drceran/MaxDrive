e693107064bcedf46f7eee7d098338b11182ff65
lrningcode12 <117692502+lrningcode12@users.noreply.github.com>
2023-04-24 23:32:03 -0700

    Created functions structure within views

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+@require_http_methods(["GET", "POST"])
+def technicians_list(request):
+    pass
+
+@require_http_methods(["DELETE"])
+def technician_detail(request, id):
+    pass
+
+@require_http_methods(["GET", "POST"])
+def appointments_list(request):
+    pass
+
+@require_http_methods(["DELETE"])
+def appointments_detail(request, id):
+    pass
+
+
+@require_http_methods(["PUT"])
+def cancel_appointment(request, id):
+    pass
+
+@require_http_methods(["PUT"])
+def finished_appointment(request, id):
+    pass
