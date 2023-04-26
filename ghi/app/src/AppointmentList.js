4275435e45024bcb32ada9777701910087454a27
lrningcode12 <117692502+lrningcode12@users.noreply.github.com>
2023-04-25 22:14:37 -0700

    Completed AppointmentList functionalities with buttons

--- ghi/app/src/AppointmentList.js
+++ ghi/app/src/AppointmentList.js
@@ -12,7 +12,30 @@ export default function AppointmentList(){
     }
 
     useEffect(()=> {loadAppointments()},[])
-    console.log(appointments)
+
+    const handleCancelUpdate = async (id) =>{
+        const url = `http://localhost:8080/api/appointments/${id}/cancel/`;
+        const fetchConfig = {
+            method: "PUT"
+        }
+        const response = await fetch(url, fetchConfig);
+        if (response.ok){
+            loadAppointments()
+        }
+        console.log(appointments)
+    }
+
+    const handleFinishUpdate = async (id) =>{
+        const url = `http://localhost:8080/api/appointments/${id}/finish/`;
+        const fetchConfig = {
+            method: "PUT"
+        }
+        const response = await fetch(url, fetchConfig);
+        if (response.ok){
+            loadAppointments()
+        }
+        console.log(appointments)
+    }
 
     return(
         <table className="table table-striped">
@@ -28,7 +51,7 @@ export default function AppointmentList(){
                 </tr>
             </thead>
             <tbody>
-                {appointments.map(appointment => (
+                {appointments.filter(appointment => appointment.status !== 'cancel' && appointment.status !== 'finish').map(appointment => (
                     <tr key={ appointment.id }>
                         <td>{appointment.vin}</td>
                         <td>Placeholder</td>
@@ -37,6 +60,12 @@ export default function AppointmentList(){
                         <td>{new Date(appointment.date_time).toLocaleTimeString()}</td>
                         <td>{appointment.technician.first_name} {appointment.technician.last_name}</td>
                         <td>{appointment.reason}</td>
+                        <td>
+                            <button type="button" className="btn btn-primary" name="cancel-button" onClick={()=>handleCancelUpdate(appointment.id)}>Cancel</button>
+                        </td>
+                        <td>
+                            <button type="button" className="btn btn-primary" name="finish-button" onClick={()=>handleFinishUpdate(appointment.id)}>Finish</button>
+                        </td>
                     </tr>
                 ))}
             </tbody>
