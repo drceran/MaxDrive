a5f0d4b31f6c1c7b3078a36f626a633a87dea67e
lrningcode12 <117692502+lrningcode12@users.noreply.github.com>
2023-04-28 15:57:03 -0700

    Cleaned up non-additive comments in code

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Technician(models.Model):
     first_name = models.CharField(max_length=100)
     last_name= models.CharField(max_length=100)
